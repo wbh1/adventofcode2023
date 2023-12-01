b433711080ac6157e73187dd94b365352ae28723
Will Hegedus <whegedus@linode.com>
2023-12-01 11:19:08 -0500

    ref(day1): simplify solutions

--- solutions/day1.py
+++ solutions/day1.py
@@ -2,11 +2,23 @@ from solutions.Puzzle import Puzzle
 from typing import List
 import re
 
+
 class Day1(Puzzle):
     day = 1
-    num_re = re.compile(r'(\d)')
-    calibration_values : List[int] = []
-    nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    num_re = re.compile(r"(\d)")
+    calibration_values: List[int] = []
+    nums = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ]
 
     def _to_int(self, num: str) -> int:
         try:
@@ -14,23 +26,20 @@ class Day1(Puzzle):
         except ValueError:
             return int(num)
 
+    def _parse_line(self, line) -> int:
+        g = self.num_re.findall(line)
+        if len(g) < 1:
+            raise ValueError(f"No numbers in line: {line}")
+        return int(f"{self._to_int(g[0])}{self._to_int(g[-1])}")
+
     def part1(self) -> int:
-        for line in self.data:
-            g = self.num_re.findall(line)
-            if len(g) < 1:
-                raise ValueError(f"No numbers in line: {line}")
-            self.calibration_values.append(int(f"{g[0]}{g[-1]}"))
+        self.calibration_values = [self._parse_line(line) for line in self.data]
         return sum(self.calibration_values)
 
     def part2(self) -> int:
-        # Reset array
-        self.calibration_values = []
         # Regex must use positive lookahead so that "sevenine" returns "79"
-        self.num_re = re.compile(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))')
-        for line in self.data:
-            g = self.num_re.findall(line)
-            if len(g) < 1:
-                raise ValueError(f"No numbers in line: {line}")
-            self.calibration_values.append(int(f"{self._to_int(g[0])}{self._to_int(g[-1])}"))
+        self.num_re = re.compile(
+            r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"
+        )
+        self.calibration_values = [self._parse_line(line) for line in self.data]
         return sum(self.calibration_values)
-
