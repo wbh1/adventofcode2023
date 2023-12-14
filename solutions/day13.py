c3ca3fcb715541ec5f0c291b159dd8c90fee17f4
Will Hegedus <whegedus@linode.com>
2023-12-14 09:50:50 -0500

    ref(day13): cleanup code

--- solutions/day13.py
+++ solutions/day13.py
@@ -1,5 +1,6 @@
+from typing import AnyStr, List, Optional, Tuple
+
 from solutions.Puzzle import Puzzle
-from typing import List, Tuple, Optional, Iterable
 
 
 class Day13(Puzzle):
@@ -9,22 +10,30 @@ class Day13(Puzzle):
         super().__init__(data)
         self.images: List[List[str]] = []
         cursor = 0
+        # Iterate over the data, which is already split by line
+        # but we need to identify empty lines that separate images
         for i, line in enumerate(self.data):
             if len(line) < 1:
                 self.images.append(list(self.data[cursor:i]))
-                cursor = i+1
+                cursor = i + 1
+        # if there's any remaining non-empty data that hasn't been added
+        # as an image yet, add it now
         if any(self.data[cursor:]):
             self.images.append(self.data[cursor:])
-        self.verticals = []
-        self.horizontals = []
 
     @staticmethod
-    def horizontal_reflection(image: List[Iterable], fix_smudges=False) -> Tuple[bool, Optional[int]]:
-        for ri, row in enumerate(image[:len(image)-1]):
+    def horizontal_reflection(
+        image: List[AnyStr], fix_smudges=False
+    ) -> Tuple[bool, Optional[int]]:
+        for ri, row in enumerate(image[: len(image) - 1]):
             a = image[max(0, (ri + 1) * 2 - len(image)) : ri + 1]
             b = image[ri + 1 : ri + len(a) + 1]
+            # to be a reflection, the order of entries in b should be the inverse of a
+            # i.e. a's last item should be b's first, and so on
             b.reverse()
             assert len(a) == len(b)
+            # if we're doing part 2, identify where a and b only have
+            # one different character between the two of them
             if fix_smudges:
                 diffs = 0
                 for i in range(0, len(a)):
@@ -32,7 +41,7 @@ class Day13(Puzzle):
                         if a1 != b1:
                             diffs += 1
                     if diffs > 1:
-                        break 
+                        break
                 if diffs == 1:
                     return (True, ri + 1)
             else:
@@ -41,39 +50,43 @@ class Day13(Puzzle):
         return (False, None)
 
     @staticmethod
-    def vertical_reflection(image: List[str], fix_smudges=False) -> Tuple[bool, Optional[int]]:
-        columns: List[List[str]] = [[] for r in range(0, len(image[0]))]
+    def vertical_reflection(
+        image: List[AnyStr], fix_smudges=False
+    ) -> Tuple[bool, Optional[int]]:
+        """vertical_reflection works by changing the list of row
+        to a list of columns and then applying horizontal reflection.
+        Think of it as rotating the image 90 degrees so that the vertical
+        line of reflection is now a horizontal line.
+        """
+        columns: List[str] = ["" for r in range(0, len(image[0]))]
         for row in image:
             for i, c in enumerate(row):
-                columns[i].append(c)
+                columns[i] += str(c)
         return Day13.horizontal_reflection(columns, fix_smudges)
 
-    def part1(self) -> int:
+    def process(self, **kwargs) -> int:
         verts = 0
         hors = 0
         for i in self.images:
-            if (v := self.horizontal_reflection(i)) and v[0]:
+            if (
+                (v := self.horizontal_reflection(i, **kwargs))
+                and v[0]
+                and v[1] is not None
+            ):
                 hors += v[1]
-                self.horizontals.append(i)
-            elif (v := self.vertical_reflection(i)) and v[0]:
+            elif (
+                (v := self.vertical_reflection(i, **kwargs))
+                and v[0]
+                and v[1] is not None
+            ):
                 verts += v[1]
-                self.verticals.append(i)
             else:
                 raise ValueError("can't reflect!")
 
         return verts + (100 * hors)
+
+    def part1(self) -> int:
+        return self.process()
 
     def part2(self) -> int:
-        verts = 0
-        hors = 0
-        for i in self.images:
-            if (v := self.horizontal_reflection(i, fix_smudges=True)) and v[0]:
-                hors += v[1]
-                self.horizontals.append(i)
-            elif (v := self.vertical_reflection(i, fix_smudges=True)) and v[0]:
-                verts += v[1]
-                self.verticals.append(i)
-            else:
-                raise ValueError("can't reflect!")
-
-        return verts + (100 * hors)
+        return self.process(fix_smudges=True)
